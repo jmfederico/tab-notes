c655ab8bc8a866aaca6c9bec7f8875a9a26f623f
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2017-03-12 13:52:44 -0500

    Comments cleanup

--- main.js
+++ main.js
@@ -182,8 +182,8 @@
     $wrapper.toggleClass('open')
   })
 
+  // When enabling preview, add class to editor, so i can be hidden via CSS.
   window.SimpleMDE.prototype.togglePreviewOld = window.SimpleMDE.prototype.togglePreview
-
   window.SimpleMDE.prototype.togglePreview = function () {
     var cm = this.codemirror
     var wrapper = cm.getWrapperElement()
@@ -215,9 +215,7 @@
    */
   function toggleSignIn () {
     if (firebase.auth().currentUser) {
-      // [START signout]
       firebase.auth().signOut()
-    // [END signout]
     } else {
       var email = document.getElementById('email').value
       var password = document.getElementById('password').value
@@ -230,7 +228,6 @@
         return
       }
       // Sign in with email and pass.
-      // [START authwithemail]
       firebase.auth().signInWithEmailAndPassword(email, password).then(function (user) {
         database.ref('users/' + user.uid + '/note/content').once('value').then(function (snapshot) {
           // Mix Local and Cloud notes.
@@ -260,10 +257,8 @@
           $('#sync-status').click()
         })
       }).catch(function (error) {
-        // Handle Errors here.
         var errorCode = error.code
         var errorMessage = error.message
-        // [START_EXCLUDE]
         if (errorCode === 'auth/wrong-password') {
           window.alert('Wrong password.')
         } else {
@@ -271,9 +266,7 @@
         }
         console.log(error)
         document.getElementById('quickstart-sign-in').disabled = false
-      // [END_EXCLUDE]
       })
-    // [END authwithemail]
     }
     document.getElementById('quickstart-sign-in').disabled = true
   }
@@ -293,7 +286,6 @@
       return
     }
     // Sign in with email and pass.
-    // [START createwithemail]
     firebase.auth().createUserWithEmailAndPassword(email, password).then(function (user) {
       if (!user.emailVerified) {
         sendEmailVerification()
@@ -302,54 +294,41 @@
       // Handle Errors here.
       var errorCode = error.code
       var errorMessage = error.message
-      // [START_EXCLUDE]
       if (errorCode === 'auth/weak-password') {
         window.alert('The password is too weak.')
       } else {
         window.alert(errorMessage)
       }
       console.log(error)
-    // [END_EXCLUDE]
     })
-  // [END createwithemail]
   }
 
   /**
    * Sends an email verification to the user.
    */
   function sendEmailVerification () {
-    // [START sendemailverification]
     firebase.auth().currentUser.sendEmailVerification().then(function () {
       // Email Verification sent!
-      // [START_EXCLUDE]
       window.alert('Email Verification Sent!')
-    // [END_EXCLUDE]
     })
-  // [END sendemailverification]
   }
 
   function sendPasswordReset () {
     var email = document.getElementById('email').value
-    // [START sendpasswordemail]
     firebase.auth().sendPasswordResetEmail(email).then(function () {
       // Password Reset Email Sent!
-      // [START_EXCLUDE]
       window.alert('Password Reset Email Sent!')
-    // [END_EXCLUDE]
     }).catch(function (error) {
       // Handle Errors here.
       var errorCode = error.code
       var errorMessage = error.message
-      // [START_EXCLUDE]
       if (errorCode === 'auth/invalid-email') {
         window.alert(errorMessage)
       } else if (errorCode === 'auth/user-not-found') {
         window.alert(errorMessage)
       }
       console.log(error)
-    // [END_EXCLUDE]
     })
-  // [END sendpasswordemail]
   }
 
   /**
@@ -359,15 +338,11 @@
    */
   function initApp () {
     // Listening for auth state changes.
-    // [START authstatelistener]
     firebase.auth().onAuthStateChanged(function (user) {
-      // [START_EXCLUDE silent]
       document.getElementById('quickstart-verify-email').disabled = true
-      // [END_EXCLUDE]
       if (user) {
         // User is signed in.
         var emailVerified = user.emailVerified
-        // [START_EXCLUDE silent]
         document.getElementById('quickstart-sign-in').textContent = 'Sign out'
         document.getElementById('email').disabled = true
         document.getElementById('password').disabled = true
@@ -376,23 +351,17 @@
         if (!emailVerified) {
           document.getElementById('quickstart-verify-email').disabled = false
         }
-      // [END_EXCLUDE]
       } else {
         // User is signed out.
-        // [START_EXCLUDE silent]
         document.getElementById('quickstart-sign-in').textContent = 'Sign in'
         document.getElementById('email').disabled = false
         document.getElementById('password').disabled = false
         document.getElementById('quickstart-sign-up').disabled = false
         document.getElementById('quickstart-password-reset').disabled = false
-      // [END_EXCLUDE]
       }
-      // [START_EXCLUDE silent]
       document.getElementById('quickstart-sign-in').disabled = false
-    // [END_EXCLUDE]
       toggleSyncStatus()
     })
-    // [END authstatelistener]
 
     var connectedRef = firebase.database().ref('.info/connected')
     connectedRef.on('value', function (snap) {
